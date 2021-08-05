f5831194e23af22a5658ae1dfc45381bc16d2fc7
Balázs Gyulai <trombita0531@gmail.com>
2021-08-05 11:49:15 +0200

    hsv to rgb

---
+++ test.py
@@ -0,0 +1,22 @@
+import math
+def hsv2rgb(h, s, v):
+    h = float(h)
+    s = float(s)
+    v = float(v)
+    h60 = h / 60.0
+    h60f = math.floor(h60)
+    hi = int(h60f) % 6
+    f = h60 - h60f
+    p = v * (1 - s)
+    q = v * (1 - f * s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = 0, 0, 0
+    if hi == 0: r, g, b = v, t, p
+    elif hi == 1: r, g, b = q, v, p
+    elif hi == 2: r, g, b = p, v, t
+    elif hi == 3: r, g, b = p, q, v
+    elif hi == 4: r, g, b = t, p, v
+    elif hi == 5: r, g, b = v, p, q
+    r, g, b = round(int(r * 255)), round(int(g * 255)), round(int(b * 255))
+    print (r,g,b)
+hsv2rgb(0,0.5,0.5)
